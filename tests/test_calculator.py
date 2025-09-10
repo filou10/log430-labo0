1977bd9b1481adbad96c4f9a7b4786ed2ccf677e
Felix-Antoine Legault <legault63@gmail.com>
2025-09-10 09:55:42 -0400

    Added test suites for the calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,12 +4,44 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
+import pytest
+
 from calculator import Calculator
 
 
-def test_app():
-    my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+@pytest.fixture(name="calculator")
+def fixture_calculator():
+    return Calculator()
 
 
-# TODO: ajoutez les tests
+def test_app(calculator: Calculator):
+
+    EXPECTED_HELLO_MSG = "== Calculatrice v1.0 =="
+
+    assert EXPECTED_HELLO_MSG == calculator.get_hello_message()
+
+
+def test_addition(calculator: Calculator):
+    assert calculator.addition(2, 3) == 5
+    assert calculator.last_result == 5
+
+
+def test_subtraction(calculator: Calculator):
+    assert calculator.subtraction(10, 3) == 7
+    assert calculator.last_result == 7
+
+
+def test_multiplication(calculator: Calculator):
+    assert calculator.multiplication(4, 3) == 12
+    assert calculator.last_result == 12
+
+
+def test_division_normal(calculator: Calculator):
+    assert calculator.division(10, 2) == 5
+    assert calculator.last_result == 5
+
+
+def test_division_by_zero(calculator: Calculator):
+    result = calculator.division(10, 0)
+    assert result == "Erreur : division par zéro"
+    assert calculator.last_result == "Error"
