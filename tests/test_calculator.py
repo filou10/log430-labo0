f83937c0f6d544d0da3bb971cc88e91c3773e3e4
Felix-Antoine Legault <legault63@gmail.com>
2025-09-10 09:43:12 -0400

    Added pyproject and fixed tests import + moved test out of src

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,8 +6,10 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 
 from calculator import Calculator
 
+
 def test_app():
     my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "Calculatrice"
+
 
 # TODO: ajoutez les tests
